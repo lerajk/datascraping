9db832285f96686e59dc9312ac0994403cf94d90
lerajk <lerajk44@gmail.com>
2017-08-01 13:49:57 -0400

    intial

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+// require csvtojson
+var csv = require("csvtojson");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+
+// Convert a csv file with csvtojson
+csv()
+  .fromFile('./csv/data.csv')
+  .on("end_parsed",function(jsonArrayObj){ //when parse finished, result will be emitted here.
+    // console.log(jsonArrayObj); 
+     res.send(jsonArrayObj);
+   })
+
+
+});
+
+module.exports = router;
